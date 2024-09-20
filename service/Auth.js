0f7975b40f60477fa27b956d138f25a34e7275c4
ASHISH KUMAR DUBEY <ashishdubeyak04@gmail.com>
2024-09-20 11:49:48 +0530

    final

--- service/Auth.js
+++ service/Auth.js
@@ -1,14 +1,19 @@
-const sessionIdToUser = new Map();    // a hash map
+const jwt = require("jsonwebtoken")
+const secret = "Ashiish._."
 
 
-// This will set the session id to the user
-function setUser(id , user) {
-    sessionIdToUser.set(id, user);
+// This will create the tokens for the users
+function setUser(user) {
+    return jwt.sign({
+        _id: user.id,
+        email: user.email,
+    }, secret);
 }
 
 // This will get the user with the required id
-function getUser(id) {
-    return sessionIdToUser.get(id);
+function getUser(token) {
+    if(!token) return null; 
+    return jwt.verify(token, secret);
 }
 
 module.exports = {
